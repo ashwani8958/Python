8d45ae5ca63657a532249d7095c5efa71c2be0d6
Ashwani Kumar <53987093+ashwani8958@users.noreply.github.com>
2020-05-19 22:15:22 +0530

    commit

---
+++ Image Processing/Udemy Digital Image Processing/0_Basic/1_print.py
@@ -0,0 +1,24 @@
+
+
+m = 0
+
+print("{} is a value of m ".format(m))
+
+
+mylist =[]
+
+mylist.append(56)
+mylist.append(56)
+mylist.append(56)
+
+print(mylist)
+
+for x in mylist:
+    print(x)
+
+list1 = [1,2,3,4,5]
+list2 = [6,7,8,9,0]
+
+print(list1 + list2)
+
+##print(list1 * list2) ## error
