db630461694cc2478aab21998b899d4feb85654e
Ashwani Kumar <53987093+ashwani8958@users.noreply.github.com>
2020-05-22 15:51:38 +0530

    commit

---
+++ Image Processing/Udemy Digital Image Processing/5_NeighborhoodProcessing/3_MinFilter.py
@@ -0,0 +1,12 @@
+import scipy.misc
+import scipy.ndimage
+from PIL import Image
+
+a  = Image.open('../images/lena512.bmp')
+
+b = scipy.ndimage.filters.minimum_filter(a,size=5, footprint = None, output = None, mode= 'reflect', cval =0.0,origin =0)
+
+b = Image.fromarray(b)
+# b = scipy.misc.toimage(b)
+
+b.show()
