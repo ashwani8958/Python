cf4934ef5bc7a24565c436c5125a3906886e3041
Ashwani Kumar <53987093+ashwani8958@users.noreply.github.com>
2020-05-24 13:15:08 +0530

    edit for smile detection

--- Image Processing/Computer_Vision_A_Z/Module 1 - Face Recognition/face_recognition_nocomment.py
+++ Image Processing/Computer_Vision_A_Z/Module 1 - Face Recognition/face_recognition_nocomment.py
@@ -18,6 +18,10 @@ def detect(gray, frame):
         eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        # Added later for mouth detections
+        smile = smile_cascade.detectMultiScale(roi_gray, 1.1, 3)
+        for (sx,sy,sw,sh) in smile:
+            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
     return frame
 
 # Doing some Face Recognition with the webcam
