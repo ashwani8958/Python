7f84a12cf7a6d06d109232068e5881246aceeb75
Ashwani Kumar <53987093+ashwani8958@users.noreply.github.com>
2020-05-24 16:46:02 +0530

    commit

--- Image Processing/Computer_Vision_A_Z/Face Recognition/3_FaceRecognitionInImages.py
+++ Image Processing/Computer_Vision_A_Z/Face Recognition/3_FaceRecognitionInImages.py
@@ -16,13 +16,28 @@ eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
 def detect(gray, frame):
-    faces = face_cascade.detectMultiScale(gray, 2.1, 10)
+    
+    # detect face
+    faces = face_cascade.detectMultiScale(gray, 1.2, 20)
     for (x,y,w,h) in faces:
         cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        
+        # In face detect eye
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 7)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        
+        # In face detect smile
+        smile = smile_cascade.detectMultiScale(roi_gray, 1.7, 20)
+        for (sx,sy,sw,sh) in smile:
+            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
+            
     return frame
 
 
-image = cv2.imread('../images/person.jpg')
+image = cv2.imread('../images/threeperson.jpg')
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 canvas = detect(gray, image)
